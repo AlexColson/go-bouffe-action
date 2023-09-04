c4d3faebb13fbef7c169aee8f3146df529e90c4f
Alex Colson <alex.colson@gmail.com>
2023-09-04 15:47:04 -0400

    Add methods to interact with records in db

--- db.go
+++ db.go
@@ -8,13 +8,14 @@ import (
 )
 
 type Record struct {
-	gorm.Model
-	Id        int
-	Provider  string
-	Product   string
-	Weigth    float32
-	Quantity  int8
-	Timestamp time.Time
+	// gorm.Model
+	Id        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Provider  string    `json:"provider"`
+	Product   string    `json:"product"`
+	Weight    float64   `json:"weight"`
+	Quantity  int       `json:"quantity"`
+	Timestamp time.Time `json:"timestamp"`
+	Comment   string    `json:"comment"`
 }
 
 func GetDbSession() *gorm.DB {
@@ -29,4 +30,35 @@ func GetDbSession() *gorm.DB {
 
 	return session
 
+}
+
+func GetRecords(db *gorm.DB, fromDate string) ([]Record, error) {
+	var records []Record
+	query := db.Order("id desc")
+
+	if fromDate != "" {
+		query = query.Where("DATE(timestamp) = ?", fromDate)
+	}
+
+	if err := query.Find(&records).Error; err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func DeleteRecord(db *gorm.DB, id uint) error {
+	return db.Delete(&Record{}, id).Error
+}
+
+func UpdateRecord(db *gorm.DB, id uint, quantity int, comment string) error {
+	updates := map[string]interface{}{}
+	if quantity != 0 {
+		updates["quantity"] = quantity
+	}
+	if comment != "" {
+		updates["comment"] = comment
+	}
+
+	return db.Model(&Record{}).Where("id = ?", id).Updates(updates).Error
 }
